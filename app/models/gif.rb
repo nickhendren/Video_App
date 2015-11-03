a2dd7deb2bbd56d33b824f2da69451d8bb98a40c
nickhendren <nhendren7@gmail.com>
2015-11-03 11:04:06 -0800

    Updated the models

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,2 +1,3 @@
 class Gif < ActiveRecord::Base
+    belongs_to :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102232710) do
+ActiveRecord::Schema.define(version: 20151103190050) do
 
   create_table "gifs", force: :cascade do |t|
     t.text     "gif_url"
@@ -25,8 +25,9 @@ ActiveRecord::Schema.define(version: 20151102232710) do
     t.string   "last_name"
     t.text     "user_name"
     t.text     "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   create_table "videos", force: :cascade do |t|
