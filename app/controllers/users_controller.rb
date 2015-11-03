423134aeb822534756cd327f4c055e91a3aba1b9
nickhendren <nhendren7@gmail.com>
2015-11-03 11:25:52 -0800

    Updated User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,47 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to '/'
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.save
+      redirect_to '/'
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      redirect_to '/'
+    else
+      render 'show'
+    end
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
 end
